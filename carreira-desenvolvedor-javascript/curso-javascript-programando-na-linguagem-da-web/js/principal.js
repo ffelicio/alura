b7393c7317acc3adc73f0e74f1dfe08c8e373d86
Fernando Felício <fernando_felicio82@yahoo.com.br>
2017-11-15 01:47:05 -0200

    Finalização do módulo 3 referente a arrays, loops e estilos.

--- carreira-desenvolvedor-javascript/curso-javascript-programando-na-linguagem-da-web/js/principal.js
+++ carreira-desenvolvedor-javascript/curso-javascript-programando-na-linguagem-da-web/js/principal.js
@@ -1,25 +1,57 @@
-// Calcular o peso do primeiro paciente
-var paciente = document.querySelector('#primeiro-paciente'),
-    tdPeso = paciente.querySelector('.info-peso'),
-    tdAltura = paciente.querySelector('.info-altura'),
-    tdImc = paciente.querySelector('.info-imc'),
-    peso = tdPeso.textContent,
-    altura = tdAltura.textContent;
+/**
+ * querySelector = resgata somente uma valor mesmo que a informação passada no argumento seja uma classe.
+ * querySelectorAll = monta uma lista com os valores passados no argumento
+ * 
+ */
 
-// Validações
-var alturaEhValida = true;
-var pesoEhValido = true;
+var pacientes = document.querySelectorAll('.paciente');
+var totalPacientes = pacientes.length;
 
-if( altura <= 0 || altura >= 3.00 ) {
-    tdImc.textContent = 'Altura inválida!';
-    alturaEhValida = false;
+for(var i = 0; i < totalPacientes; i++) {
+    var paciente = pacientes[i],
+        tdPeso = paciente.querySelector('.info-peso'),
+        tdAltura = paciente.querySelector('.info-altura'),
+        tdImc = paciente.querySelector('.info-imc'),
+        peso = tdPeso.textContent,
+        altura = tdAltura.textContent;
+
+    // Validações
+    var alturaEhValida = true;
+    var pesoEhValido = true;
+
+    if( altura <= 0 || altura >= 3.00 ) {
+        tdImc.textContent = 'Altura inválida!';
+        alturaEhValida = false;
+        // Inclusão de css via js
+        // paciente.style.color = 'red';
+        
+        /*
+         * Para palavras compostas de estilo como por exemplo, 'background-color', o javascript não aceita essa grafia.
+         * A mesma deverá ser feita com o padrão camelCase, conforme o exemplo abaixo.
+         */
+        // paciente.style.backgroundColor = 'lightcoral';
+
+        /*
+         * Para alteração de elementos que afetam a visualização, a melhor maneira é fazer conforme descrito abaixo:
+         * 1. Criar uma regra css para alteração do elemento(html);
+         * 2. Realizar a inclusão via javascript no elemento.
+         * 
+         * Para inclusão da classe é utilizada a propriedade 'classList' no objeto. Esse atributo retorna uma lista(array) de classes
+         * apontadas no elemento. O mesmo também nos dá suporte para alguns métodos, como o mostrado abaixo, onde é adicionada uma nova classe
+         * no elemento(objeto).
+         */
+        paciente.classList.add('paciente-invalido');
+    }
+
+    if( peso <= 0 || peso >= 350 ) {
+        tdImc.textContent = 'Peso inválido!';
+        pesoEhValido = false;
+        paciente.classList.add('paciente-invalido');
+    }
+
+    if( alturaEhValida && pesoEhValido ) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
 
-if( peso <= 0 || peso >= 350 ) {
-    tdImc.textContent = 'Peso inválido!';
-    pesoEhValido = false;
-}
-
-if( alturaEhValida && pesoEhValido ) {
-    tdImc.textContent = peso / (altura * altura);
-}
